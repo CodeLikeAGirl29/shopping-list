ff3518495926bcd3048d5dc70e2020a735b941d2
Lindsey Howard <25946305+CodeLikeAGirl29@users.noreply.github.com>
2021-03-19 00:42:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-//This adds new list item to shopping list
- $("form :button").on("click", function(){
-    let task = $('.js-shopping-list-entry').val();
-    let check = "check";
-    let deleted = "delete";
-    if(task.length > 0) {
-      $(".shopping-list").append('<li><span class="shopping-item">' + task + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">' + check + '</span></button><button class="shopping-item-delete"><span class="button-label">' + deleted + '</span></button></div></li>');
-    }
-    else {
-      return;
-    }
+$(function () {
+  $('#js-shopping-list-form').submit(event => {
+    event.preventDefault();
+    //$(".js-shopping-list-entry").empty();
+
+    const item = $(this).find(
+      'input[name="shopping-list-entry"]').val();
+
+    const buttonDiv = $(this).find(
+      ".shopping-item-controls").html();
+
+    $("ul").append(
+      "<li>" +
+      //"<span class=\"shopping-item\">"+
+      '<span class = "shopping-item">' +
+      [item] +
+      "</span>" +
+      //"<div class=\"shopping-item-controls\">"+
+      '<div class = "shopping-item-controls">' +
+      // [buttonDiv] +
+      '<button class="shopping-item-toggle">' +
+      '<span class="button-label">check</span>' +
+      '</button>' +
+      '<button class="shopping-item-delete">' +
+      '<span class="button-label">delete</span>' +
+      '</button>' +
+      "</div>" +
+      "</li>");
+
+    $(".js-shopping-list-entry").val('');
+
   });
 
-$("#js-shopping-list-form").on('submit',function(e){                 
-  //Add stuff to database if necessary
-     e.preventDefault();
-     return false;
+  $('.shopping-list').on('click', '.shopping-item-toggle', function (event) {
+    const targetItem = $(event.currentTarget).closest("li");
+    targetItem.find(".shopping-item").toggleClass('shopping-item__checked');
   });
 
-  //This checks your list item
-    $("ul").on("click", ".shopping-item-toggle", function() {
-      $(this).parent().parent().find('.shopping-item').toggleClass("shopping-item__checked");
+  $('.shopping-list').on('click', '.shopping-item-delete', function (event) {
+    const delItem = $(event.currentTarget).closest("li");
+    delItem.remove();
   });
-
-//This deletes your list item
-  $("ul").on("click", ".shopping-item-delete", function() {
-      event.preventDefault();
-    $(event.target).closest("li").remove();
-  });
-
-  //this function adds an li and the text from the input box as well as clearing the box out and keeping the cursor inside of the input box
-   $('button').click(function() {
-    var text = $('#list-item').val();
-    $('.add').append('<li>' + '<input class="check" type="checkbox"/>' + text + '<button class="delete">x</button>' + '</li>');
-    $('#list-item').val('').focus();
+});
